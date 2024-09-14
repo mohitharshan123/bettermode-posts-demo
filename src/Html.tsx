77dab5aeb85b52a0a802c4fe515b47cd106fd197
Mohit <mohit@Mohits-MacBook-Pro-3.local>
2024-09-14 22:22:11 +0530

    Fixed page title

--- src/Html.tsx
+++ src/Html.tsx
@@ -12,7 +12,7 @@ const Html: React.FC<HtmlProps> = ({ isProduction, assets, children }) => {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Vite + React</title>
+        <title>Bettermode</title>
         {assets?.map((asset: string) => (
           <link
             key={asset}
